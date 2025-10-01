df12261ceb8aca6fc9ab41d9548d2bd18f43bca9
Tangi GUEVEL <t25guevel@poivre8.enib.fr>
2025-10-01 15:37:55 +0200

    first commit

---
+++ jeux_du_pendu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define TAILLE_MAX_MOT 100
+
+
+int verifier_tentative(int c,int mot){
+
+
+}
+
+int main(void){
+    int mot[TAILLE_MAX_MOT],erreur;
+    printf("joueur 1 choisisser votre mot mystére : ");
+    scanf("%s",mot);
+    printf("joueur 1 choisisser votre nombre d'érreur : ");
+    scanf("%d",&erreur);
+    printf("Le mot mystére est %s avec %d érreur \n",mot,erreur);
+    //Prochaine etape ?
+    // Tant que joueur 2 n'a pas trouvé le mot 
+        //joueur 2 fait une proposition
+    
+}

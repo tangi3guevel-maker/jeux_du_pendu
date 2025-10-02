b3c6b0c72f8de8b8389d64408b4abad1a3a8a1ac
tangi3guevel-maker <tangi3.guevel@gmail.com>
2025-10-02 13:08:52 +0200

    Update jeux_du_pendu.c

--- jeux_du_pendu.c
+++ jeux_du_pendu.c
@@ -15,13 +15,13 @@ int main(void){
     int mot[TAILLE_MAX_MOT]; // mot mystére
     int erreur; // monbre d'érreur
     int mot_touvee=0; // tanque mot mystére non trouvée = 0 sinon = 1
+    int essai; // letre chois par joueur 2
     printf("joueur 1 choisisser votre mot mystére : ");
     scanf("%s",mot);
     printf("joueur 1 choisisser votre nombre d'érreur : ");
     scanf("%d",&erreur);
     printf("Le mot mystére est %s avec %d érreur \n",mot,erreur);
     while(mot_touvee==0){
-        int essai; // letre chois par joueur 2
         printf("Joueur 2 choisiser une letre : ");
         scanf("%s \n",essai);
         if(verifier_tentative(essai,mot)==1){

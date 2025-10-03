6fcf11a8a665f3a473ae6f9d734fe02ab6fadf54
tangi3guevel-maker <tangi3.guevel@gmail.com>
2025-10-03 11:06:29 +0200

    Update jeux_du_pendu.c

--- jeux_du_pendu.c
+++ jeux_du_pendu.c
@@ -4,26 +4,34 @@
 #define TAILLE_MAX_MOT 100
 
 
-int verifier_tentative(int c,int *mot){
-    int i;
-    for(i=0;i<15;i=i+1){
-        printf("%c",mot[i]);
+int verifier_tentative(char c,char *mot){
+    int i; // valeur de la boucle
+    int est_trouve=0; // renvoie 0 si c n'est pas dans mot sinon renvoi 1
+    for(i=0;i<strlen(mot);i=i+1){
+        if(mot[i]==c){
+            est_trouve=1;
+        }
     }
+    return est_trouve;
+}
+
+int maj_mot_devin(char mot_devine,char *mot_mystere){
+    
 }
 
 int main(void){
-    int mot[TAILLE_MAX_MOT]; // mot mystére
+    char mot[TAILLE_MAX_MOT]; // mot mystére
     int erreur; // monbre d'érreur
     int mot_touvee=0; // tanque mot mystére non trouvée = 0 sinon = 1
-    int essai; // letre chois par joueur 2
     printf("joueur 1 choisisser votre mot mystére : ");
     scanf("%s",mot);
     printf("joueur 1 choisisser votre nombre d'érreur : ");
     scanf("%d",&erreur);
     printf("Le mot mystére est %s avec %d érreur \n",mot,erreur);
-    while(mot_touvee==0){
+    while(1){
+        char essai; // letre chois par joueur 2 
         printf("Joueur 2 choisiser une letre : ");
-        scanf("%s \n",essai);
+        scanf(" %c", &essai);
         if(verifier_tentative(essai,mot)==1){
             printf("oui \n");
         }

b806be12c7831abc8a723dac11c94e3147c8255d
tangi3guevel-maker <tangi3.guevel@gmail.com>
2025-10-09 09:39:39 +0200

    Update jeux_du_pendu.c

--- jeux_du_pendu.c
+++ jeux_du_pendu.c
@@ -10,37 +10,37 @@ void afficher_pendu(int erreurs_actuelles, int erreurs_max) {
     int taux_erreur = (erreurs_actuelles * 100) / erreurs_max;
     // Ajout de la barre supérieure (30%)
     if (taux_erreur >= 30) {
-        printf(" _______\n"); // Barre supérieure (transversale)
+        printf("  _______\n"); // Barre supérieure (transversale)
     }
     // Ajout de la corde (40%)
     if (taux_erreur >= 40) {
-        printf(" |/ |\n"); // Corde accrochée
+        printf("  |/ |\n"); // Corde accrochée
     } else if (taux_erreur >= 30) {
-        printf(" |/ \n"); // Barre sans corde
+        printf("  |/ \n"); // Barre sans corde
     }
     if (taux_erreur>=50){
-        printf(" | (_)\n"); // Tête du pendu
+        printf("  | (_)\n"); // Tête du pendu
     }
     else if(taux_erreur>=20){
-        printf(" |\n"); // Pilier complet (à 20%)
+        printf("  |\n"); // Pilier complet (à 20%)
     }
     // Ajout des bras (60%)
     if (taux_erreur >= 60) {
-        printf(" | \\|/\n"); // Bras complets
+        printf("  | \\|/\n"); // Bras complets
     } else if (taux_erreur >= 20) {
-        printf(" |\n"); // Pas de bras, mais espace sous le torse
+        printf("  |\n"); // Pas de bras, mais espace sous le torse
     }
     // Ajout du torse (80%)
     if (taux_erreur >= 80) {
-        printf(" | |\n"); // Torse
+        printf("  |  |\n"); // Torse
     } else if (taux_erreur >= 20) {
-        printf(" |\n"); // Pas de torse, mais espace sous la tête
+        printf("  |\n"); // Pas de torse, mais espace sous la tête
     }
     // Ajout des jambes (100%)
     if (taux_erreur >= 100) {
-        printf(" | / \\\n"); // Jambes complètes
+        printf("  | / \\\n"); // Jambes complètes
     } else if (taux_erreur >= 10) {
-        printf(" |\n"); // Pas de jambes, mais espace sous les bras
+        printf("  |\n"); // Pas de jambes, mais espace sous les bras
     }
     if (taux_erreur>=10){// Affichage de la base du pilier
         printf("__|__\n");
@@ -61,7 +61,13 @@ int maj_mot_devin(char mot_devine,char mot_mystere){
 }
 
 void afficher_mot_devine(char *mot){
-    printf("le mot mystere est %s \n",mot);
+    int b; // valeur de la boucle
+    printf("le mot mystere est : ");
+    for(b=0;b<strlen(mot);b=b+1){
+        printf("%c",mot[b]);
+        printf("%c",' ');
+    }
+    printf("\n");
 }
 
 void afficher_progression(char *mot_actuel,char *lettre_deja_fausse,int ereur_restant){
@@ -85,12 +91,10 @@ int main(void){
         mot[n]=tolower(mot[n]);
     }
     char mot_a_trouve[strlen(mot)*2]; // mot mystére pour joueur 2
-    while(j<(strlen(mot)*2)){
+    while(j<(strlen(mot))){
         mot_a_trouve[j] = '_';
-        mot_a_trouve[j+1] = ' ';
-        j=j+2;
+        j=j+1;
     }
-    printf("%s et %s \n",mot_a_trouve,mot);
     int i; // valeur de la boucle
     int k; // valeur de la boucle
     int m; // valeur de la boucle
@@ -100,48 +104,48 @@ int main(void){
     char mot_erreu[TAILLE_MAX_MOT]; // lettre fausse déjà utilisé
     char essai; // letre chois par joueur 2 
     while(mot_touvee==0){
-        
         printf("Joueur 2 choisiser une letre : ");
         scanf(" %c", &essai);
         for(i=0;i<strlen(mot);i=i+1){
-            if(verifier_tentative(essai,mot[i])==1){
+            if(verifier_tentative(essai,mot[i])==1){ // la lettre est dans le mot mystére
                 mot_a_trouve[i]=maj_mot_devin(mot_a_trouve[i],essai);
-                lettre_vrai=lettre_vrai+1;
+                lettre_vrai=lettre_vrai+1; // +1 si la lettre est dans le mot mystére
             }   
         }
         if(lettre_vrai==0){ // la lettre n'aparait pas dans le mot
             for(k=0;k<strlen(mot_erreu);k=k+1){
-                if(mot_erreu[k]==essai){ // vérifit si le mot faut n'a pas déjà été noté
-                    lettre_deja_utilise=lettre_deja_utilise+1;
+                if(mot_erreu[k]==essai){ // la letre fausse à déjà été noté
+                    lettre_deja_utilise=lettre_deja_utilise+1; // +1 si la lettre est dans la liste d'érreur
                 }
             }
-            if(lettre_deja_utilise==0){
-                mot_erreu[strlen(mot_erreu)]=essai;
-                erreur=erreur+1;
+            if(lettre_deja_utilise==0){ // si la lettre na pas été noté
+                mot_erreu[strlen(mot_erreu)]=essai; // rajoute la letre à la liste
+                erreur=erreur+1; // fait perdre une vie
             }
-        }
-        if(erreur==erreur_max){
-            printf("\nPERDU\n le mot été %s \n",mot);
-            return 0;
         }
 
         lettre_vrai=0;
         lettre_deja_utilise=0;
         
         for(m=0;m<strlen(mot);m=m+1){
-            if(mot_a_trouve[m]==mot[m]){
+            if(mot_a_trouve[m]==mot[m]){ // vérifit si le mot est bien le mot à trouvée
                 lettre_trouvee=lettre_trouvee+1;
             }
         }
-        if(lettre_trouvee == strlen(mot)){
+        if(lettre_trouvee == strlen(mot)){ // le joueur 2 à trouvée le mot mystére 
             mot_touvee=1;
         }
         lettre_trouvee=0;
 
-        afficher_progression(mot_a_trouve,mot_erreu,erreur);
+        afficher_progression(mot_a_trouve,mot_erreu,erreur_max-erreur);
         afficher_pendu(erreur,erreur_max);
+
+        if(erreur==erreur_max){ // le joueur 2 n'a plus de vie
+            printf("\nPERDU\n le mot été %s \n\n",mot);
+            return 0;
+        }
     }
-    printf("gagné");
+    printf("\ngagné\n\n");
     return 0;
 }
 
